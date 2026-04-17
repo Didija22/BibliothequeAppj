73b931d1e451b380d464e906801e244d85b90bc1
Fatoumata Bintou Traore <fimatraore24@gmail.com>
2026-04-17 21:02:18 +0000

    ajout de la classe Membre

--- src/Membre.java
+++ src/Membre.java
@@ -1,3 +1,4 @@
+public class Membre{
 // declaration des attibuts
 private String prenom;
 private String nom;
@@ -8,17 +9,17 @@ public Membre(){};
 
 //constructeur avec parametre
 public Membre(String prenom, String nom, String idMembre){
-    this.prenom = "prenom";
-    this.nom = "nom";
-    this.idMembre = "Cg4";
+    this.prenom = prenom;
+    this.nom = nom;
+    this.idMembre = idMembre;
 }
 
 // les foncctions get
-String getPrenom(){return prenom;}
-String getNom(){return nom;}
-String getIdMembre(){return idMembre;}
+public String getPrenom(){return prenom;}
+public String getNom(){return nom;}
+public String getIdMembre(){return idMembre;}
 
 //Les fonctions set
-void setPrenom(String prenom){this.prenom=prenom;}
-void setNom(String nom){this.nom=nom;}
-void setIdMembre(String Membre){this.membre=membre;}
+public void setPrenom(String prenom){this.prenom=prenom;}
+public void setNom(String nom){this.nom=nom;}
+public void setIdMembre(String idMembre){this.idMembre=idMembre;}}
